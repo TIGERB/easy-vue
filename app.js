7a71a22069211d16f03bd4d9fb4b33a0a1dbb415
tigerb <624661874@qq.com>
2016-12-08 18:08:48 +0800

    fix(app.js): require fastclick and update routes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('!style!css!ratchet-npm/dist/css/ratchet.css');// get ratchet
 require('!style!css!font-awesome/css/font-awesome.css');// get font-awesome
 require('!style!css!animate.css/animate.css');// get animate.css
 
+var FastClick = require('fastclick');
+FastClick.attach(document.body);// init fastclick
+
 var Vue　　　 = require('vue'); // get vue
 var App　　　 = require('./app.vue'); // get root module
 var store　　 = require('./store.js'); // get vuxe -> store
@@ -28,9 +31,10 @@ Vue.use(infiniteScroll)
 
 var viewPath = './src/views/'; //component src
 var routes = [
-  { path: '/', component: require(viewPath + 'pageone.vue')},
-  { path: '/two', component: require(viewPath + 'pagetwo.vue')},
-  { path: '/three', component: require(viewPath + 'pagethree.vue')}
+  { path: '/', component: require(viewPath + 'home.vue')},
+  { path: '/lists', component: require(viewPath + 'lists.vue')},
+  { path: '/option', component: require(viewPath + 'option.vue')},
+  { path: '/detail', component: require(viewPath + 'detail.vue')}
 ];
 var router = new VueRouter({
   routes: routes
